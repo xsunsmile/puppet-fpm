d7e8d3c1c715c5e6f880a247bbc28e3c62c24f1e
Hao SUN <xsunsmile@gmail.com>
2011-06-03 15:03:52 +0900

    upload debs

--- manifests/torque.pp
+++ manifests/torque.pp
@@ -6,6 +6,8 @@ define fpm::torque(
 		$package_version,
 		$build_dirname = '/tmp/build',
 		$install_dist = '/usr/local',
+		$broker_dir = "/tmp",
+		$repo,
 		$with_doc = true,
 		$with_dev = true
 ) {
@@ -13,7 +15,7 @@ define fpm::torque(
 		include torque::install
 		include torque::params
 
-		$torque_packages_broker_dir = "/etc/puppet/modules/torque/files"
+		$broker_dir = "/etc/puppet/modules/torque/files"
 		$package_dir = regsubst( $install_dist, '^/', '' )
 		$doc_dir = regsubst( "${install_dist}/man", '^/', '' )
 		$dev_dir = regsubst( "${install_dist}/include", '^/', '' )
@@ -21,8 +23,6 @@ define fpm::torque(
 		$initd_dir = "${build_dirname}/etc/init.d"
 
 		if defined(File["${package_src}"]) {
-
-			$gem_path = gem_path()
 
 			file { "${name}_${build_dirname}":
 				name => "${build_dirname}",
@@ -44,29 +44,29 @@ define fpm::torque(
 
 			exec { "${name}_build_package":
 				cwd => "${package_src}",
-				path => "${gem_path}:/usr/bin:/bin",
+				path => "${gem_bin_path}:/usr/bin:/bin",
 				command => "fpm -s dir -t ${package_type} -n ${name} -v ${package_version} -C ${build_dirname} -p ${name}-VERSION_ARCH.${package_type} ${package_dir}/bin ${package_dir}/lib ${package_dir}/sbin ${spool_dir}",
-				require => Exec["${name}_make_install"],
+				require => [ Package['fpm'], Exec["${name}_make_install"], ],
 				timeout => 0,
-				unless => "ls ${torque_packages_broker_dir}/${name}-*deb"
+				unless => "ls ${broker_dir}/${name}-*deb"
 			}
 
 			exec { "${name}_build_doc":
 				cwd => "${package_src}",
-				path => "${gem_path}:/usr/bin:/bin",
+				path => "${gem_bin_path}:/usr/bin:/bin",
 				command => "fpm -s dir -t ${package_type} -n ${name}-doc -v ${package_version} -C ${build_dirname} -p ${name}_doc-VERSION_ARCH.${package_type} ${doc_dir}",
-				require => Exec["${name}_make_install"],
+				require => [ Package['fpm'], Exec["${name}_make_install"], ],
 				timeout => 0,
-				unless => "ls ${torque_packages_broker_dir}/${name}_doc*deb"
+				unless => "ls ${broker_dir}/${name}_doc*deb"
 			}
 
 			exec { "${name}_build_dev":
 				cwd => "${package_src}",
-				path => "${gem_path}:/usr/bin:/bin",
+				path => "${gem_bin_path}:/usr/bin:/bin",
 				command => "fpm -s dir -t ${package_type} -n ${name}-dev -v ${package_version} -C ${build_dirname} -p ${name}_dev-VERSION_ARCH.${package_type} ${dev_dir}",
-				require => Exec["${name}_make_install"],
+				require => [ Package['fpm'], Exec["${name}_make_install"], ],
 				timeout => 0,
-				unless => "ls ${torque_packages_broker_dir}/${name}_dev*deb"
+				unless => "ls ${broker_dir}/${name}_dev*deb"
 			}
 
 			file { "${build_dirname}/etc":
@@ -92,17 +92,17 @@ define fpm::torque(
 
 			exec { "${name}_build_initd":
 				cwd => "${package_src}",
-				path => "${gem_path}:/usr/bin:/bin",
+				path => "${gem_bin_path}:/usr/bin:/bin",
 				command => "fpm -s dir -t ${package_type} -n ${name}-initd -v ${package_version} -C ${build_dirname} -p ${name}_initd-VERSION_ARCH.${package_type} etc/init.d",
 				timeout => 0,
-				unless => "ls ${torque_packages_broker_dir}/${name}_initd*deb",
-				require => Exec["${name}_cp_initd"],
+				unless => "ls ${broker_dir}/${name}_initd*deb",
+				require => [ Package['fpm'], Exec["${name}_cp_initd"], ],
 			}
 
 			exec { "${name}_store_build_package":
 				cwd => "${package_src}",
 				path => "/usr/bin:/bin",
-				command => "mv *deb ${torque_packages_broker_dir}",
+				command => "mv *deb ${broker_dir}",
 				require => [
 					Exec["${name}_build_package"],
 					Exec["${name}_build_doc"],
@@ -111,6 +111,9 @@ define fpm::torque(
 				],
 				onlyif => "ls ${package_src}/*deb",
 			}
+
+			# $debs = mongo_putfiles($repo)
+			# notify{"$hostname execute ${repo} => ${debs}":}
 
 		} else {
 			alert("File['#{$package_src}'] is not defined")
